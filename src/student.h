77660011bf50ff51c360527c96e6b0ca451eb531
kmgregory <kamrengregory@gmail.com>
2025-03-14 00:28:46 -0300

    Revert "Add the Student Structure"
    This reverts commit 9a7799ef014657741111ac765637b3e5616c5eaa.

---
+++ src/student.h
@@ -0,0 +1,21 @@
+/* student.h
+ * 
+ * By Group 8:
+ * 	Kamren M. Gregory - 3759472
+ * 	Anush Matevosyan - 3758790
+ * 	Sharmila Sadia Ahmed - 3741966
+ *
+ */
+
+#ifndef STUDENT_H
+#define STUDENT_H
+
+typedef struct {
+	int id;
+	char * name;
+	double *grades;
+	double gpa;
+
+} Student;
+
+#endif
